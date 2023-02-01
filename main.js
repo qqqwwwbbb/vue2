538ad709f3be1ca5a12a72780927bb52dbb29751
qqqwwwbbb <usecaut@gmail.com>
2023-02-01 23:57:14 +0700

    mounted + methods

--- main.js
+++ main.js
@@ -63,6 +63,67 @@ Vue.component('cols', {
             errors: []
         }
     },
+    methods: {
+        newStatus(card, t) {
+            t.completed = true
+            let count = 0
+
+            for (let i = 0; i < 5; i++) {
+                if (card.subtasks[i].title != null) {
+                    count++
+                    if (card.subtasks[i].completed) {
+                        card.status++
+                    }
+                }
+            }
+
+            if (card.status/count*100 >= 50) {
+                this.column2.push(card)
+                this.column1.splice(this.column1.indexOf(card), 1)
+            } else if (card.status/count*100 === 100) {
+                this.column3.push(card)
+                this.column2.splice(this.column2.indexOf(card), 1)
+                this.column3.card.date = new Date()
+            }
+
+        }
+    },
+    props: {
+        card: {
+            title: {
+                type: Text,
+                required: true
+            },
+            subtasks: {
+                type: Array,
+                required: true,
+                completed: {
+                    type: Boolean,
+                    required: true
+                }
+            },
+            date: {
+                type: Date,
+                required: false
+            },
+            status: {
+                type: Number,
+                required: true
+            }
+        },
+
+    },
+    mounted() {
+        eventBus.$on('card-submitted', card => {
+            this.errors = []
+            if (this.column1.length < 3){
+
+                this.column1.push(card)
+            } else {
+                this.errors.push("Больше добавлять нельзя.")
+            }
+        })
+    },
 })
 
 
